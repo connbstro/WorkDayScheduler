bb249a26fbd963a9f30939c7981c7f6282820c9b
Connor S <constroh@gmail.com>
2023-05-03 10:49:23 -0500

    JS updated

--- Develop/script.js
+++ Develop/script.js
@@ -1,27 +1,25 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-  // Show current date on page.
-  
-  const currentDay = dayjs();
-  $("#currentDay").text(currentDay.format("dddd, MMMM D YYYY"));
-
-  // Time slot function for past, present, future.
-  function timeSlot() {
-    const currentHour = dayjs().hour();
-    const timeBlock = $(".time-block");
-    timeBlock.each(function () {
-      let hour = $(this).attr("id");
-      if (hour == currentHour) {
-        $(this).addClass("present");
-      } else if (hour < currentHour) {
-        $(this).addClass("past");
-      } else if (hour > currentHour) {
-        $(this).addClass("future");
-      }
-    });
-  }
-
   timeSlot();
 });
+
+// Show current date on page.
+function currentDate() {
+  const currentDay = dayjs();
+  $("#currentDay").text(currentDay.format("dddd, MMMM D YYYY"));
+}
+
+// Time slot function for past, present, future.
+function timeSlot() {
+  const currentHour = dayjs().hour();
+  const timeBlock = $(".time-block");
+  timeBlock.each(function () {
+    let hour = $(this).attr("id");
+    if (hour == currentHour) {
+      $(this).addClass("present");
+    } else if (hour < currentHour) {
+      $(this).addClass("past");
+    } else if (hour > currentHour) {
+      $(this).addClass("future");
+    }
+  });
+}

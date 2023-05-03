dba2f25721f6be87443a3ed4d9d351b14ebea971
Connor S <constroh@gmail.com>
2023-05-03 10:58:16 -0500

    local storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,8 @@
 $(function () {
   timeSlot();
+  currentDate();
+  todos();
+  saveTodo();
 });
 
 // Show current date on page.
@@ -21,5 +24,28 @@ function timeSlot() {
     } else if (hour > currentHour) {
       $(this).addClass("future");
     }
+  });
+}
+
+// Get todos from local storage.
+function todos() {
+  const timeBlock = $(".time-block");
+  timeBlock.each(function () {
+    const hour = $(this).attr("id");
+    const task = JSON.parse(localStorage.getItem(hour));
+    if (task) {
+      $(this).children("textarea").val(task);
+    }
+  });
+}
+
+// Save to local storage.
+function saveTodo() {
+  const saveBtn = $(".saveBtn");
+  saveBtn.click(function (e) {
+    e.preventDefault();
+    const hour = $(this).parents().attr("id");
+    const description = $(this).prev().val();
+    localStorage.setItem(hour, JSON.stringify(description));
   });
 }
